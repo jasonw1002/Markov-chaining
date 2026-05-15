f3919aa327d631d0f95f756835c5523254fddbd3
jasonw1002 <jasontufts2026@gmail.com>
2026-05-14 21:34:52 -0400

    Create globals.cpp

---
+++ globals.cpp
@@ -0,0 +1,12 @@
+#pragma once
+
+
+const int MAX_VELOCITY = 200;
+
+//number of training samples for birds and planes each
+const int NUMBER_OF_SAMPLES = 10;
+const int NUMBER_OF_TESTS = 10;
+
+//number of seconds each sample and test lasts
+const int TRACK_DURATION = 600;
+
